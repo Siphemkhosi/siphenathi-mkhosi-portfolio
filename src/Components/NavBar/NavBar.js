a241cdc3c4c06eeb0f8b87b57081b0a5cb15276c
MM-Maketela <maketelamafa2@gmail.com>
2022-11-21 21:08:29 +0200

    updates

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,10 +5,13 @@ import linkedIn from "../../Assets/Images/linkedin.png";
 import instagram from "../../Assets/Images/instagram.png";
 import classes from "../../Pages/LandingPage/LandingPage.module.css";
 import logo from "../../Assets/Images/logo.jpg";
+import    "./NavBarLines.css";
 
 
 const NavBar = () => {
   const [colorChange, setColorchange] = useState(false);
+  const [onPagesHover, setOnPagesHover] = useState(0);
+
   const changeNavbarColor = () => {
     if (window.scrollY >= 1) {
       setColorchange(true);
@@ -54,10 +57,82 @@ const NavBar = () => {
           </div>
 
           <div className={classes.pages}>
-            <div>Home</div>
-            <div>About</div>
-            <div>Portfolio</div>
-            <div>contact</div>
+            <div
+              onMouseEnter={function () {
+                setOnPagesHover(1);
+              }}
+              onMouseLeave={function () {
+                setOnPagesHover(-1);
+              }}
+            >
+              <div>Home</div>
+              <div
+                className={
+                  onPagesHover === 1
+                    ? "Line2"
+                    :onPagesHover === -1 ? "Line1" : "Line3"
+               
+                }
+              ></div>
+            </div>
+
+            <div
+              onMouseEnter={() => {
+                setOnPagesHover(2);
+              }}
+              onMouseLeave={() => {
+                setOnPagesHover(-2);
+              }}
+            >
+              <div>About</div>
+              <div
+                className={
+                  onPagesHover === 2
+                    ? "Line2"
+                    :onPagesHover === -2 ? "Line1" : "Line3"
+                      
+                }
+              ></div>
+            </div>
+
+            <di
+              onMouseEnter={() => {
+                setOnPagesHover(3);
+              }}
+              onMouseLeave={() => {
+                setOnPagesHover(-3);
+              }}
+            >
+              <div>Portfolio</div>
+              <div
+                className={
+                  onPagesHover === 3
+                    ? "Line2"
+                    :onPagesHover === -3 ? "Line1" : "Line3"
+                      
+                }
+              ></div>
+            </di>
+
+            <div
+              onMouseEnter={() => {
+                setOnPagesHover(4);
+              }}
+              onMouseLeave={() => {
+                setOnPagesHover(-4);
+          
+              }}
+            >
+              <div>contact</div>
+              <div
+                className={
+                  onPagesHover === 4
+                    ? "Line2"
+                    :onPagesHover === -4 ? "Line1" : "Line3"
+                    }
+                
+              ></div>
+            </div>
           </div>
         </div>
       </nav>
