60d1e5f26faba26f2e7c50ffc011a389c9405e45
MM-Maketela <maketelamafa2@gmail.com>
2022-11-22 13:26:41 +0200

    updates

--- src/Pages/LandingPage/Section2.js
+++ src/Pages/LandingPage/Section2.js
@@ -14,15 +14,17 @@ export default class Section2 extends Component {
               <div className={classes.aboutHeadingText}>About</div>
               <div className={classes.divider}></div>
               <div className={classes.infoAboutMafa}>
-                Lorem Ipsum dolor Lorem Ipsum dolor sit amet, consectetur
-                adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. 
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                C/O https://placeholder.com/text/lorem-ipsum/
+                <div className={classes.infoAboutMafa1}>
+                  Lorem Ipsum dolor Lorem Ipsum dolor sit amet, consectetur
+                  adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. enim ad minim veniam, quis nostrud
+                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                  sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum. C/O
+                  https://placeholder.com/text/lorem-ipsum/
+                </div>
               </div>
               <div className={classes.myButton}>
                 <button>Learn more</button>
